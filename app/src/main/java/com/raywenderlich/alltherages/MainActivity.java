3ec32bb4cfea64b36bbe741f85602f6a8c211928
Bryon Nicoson <bryon.nicoson@gmail.com>
2017-08-04 16:23:04 -0500

    fragmented

--- app/src/main/java/com/raywenderlich/alltherages/MainActivity.java
+++ app/src/main/java/com/raywenderlich/alltherages/MainActivity.java
@@ -24,11 +24,30 @@ package com.raywenderlich.alltherages;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements RageComicListFragment.OnRageComicSelected  {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    if (savedInstanceState == null) {
+      getSupportFragmentManager()
+              .beginTransaction()
+              .add(R.id.root_layout, RageComicListFragment.newInstance(), "rageComicList")
+              .commit();
+    }
   }
+    @Override
+    public void onRageComicSelected(int imageResId, String name, String description, String url) {
+        final RageComicDetailsFragment detailsFragment =
+                RageComicDetailsFragment.newInstance(imageResId, name, description, url);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.root_layout, detailsFragment, "rageComicDetails")
+                .addToBackStack(null)
+                .commit();
+    }
 }
